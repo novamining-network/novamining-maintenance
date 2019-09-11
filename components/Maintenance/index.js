88dcd44823b34657ba9cbae6b3267d02b152f062
Mattia Pintus <pntmtt86@gmail.com>
2019-09-11 23:12:19 +0200

    Updated file locations

--- components/Maintenance/index.js
+++ components/Maintenance/index.js
@@ -22,70 +22,72 @@ class Maintenance extends Component {
               />
             </div>
             <br/>
-            <h2 className="subtitle is-4 has-text-white has-text-centered">
-              Our website is currently down for maintenance 🤓 We're working hard to bring it back online as soon as possible 🚀
-            </h2>
-            <h2 className="subtitle is-5 has-text-white has-text-centered">
-              Do you need any further information or do you have any enquiry? 🤔 Feel free to write us at <Link href="mailto:contact@novamining.io"><a className="has-text-white">contact@novamining.io</a></Link> 🤗
-            </h2>
-            <p className="column has-text-centered-mobile">
-                <Link href="https://twitter.com/NovaMining_Net">
-                <a target="_blank" className="ls-social-icon has-text-white">
-                  <span className="icon is-large">
-                    <i className="fab fa-2x fa-twitter"></i>
-                  </span>
-                </a>
-                </Link>
-                <Link href="https://www.linkedin.com/company/novaminingnetwork">
-                <a target="_blank" className="ls-social-icon has-text-white">
-                  <span className="icon is-large">
-                    <i className="fab fa-2x fa-linkedin-in"></i>
-                  </span>
-                </a>
-                </Link>
-                <Link href="https://www.facebook.com/novamining.network">
-                <a target="_blank" className="ls-social-icon has-text-white">
-                  <span className="icon is-large">
-                    <i className="fab fa-2x fa-facebook-f"></i>
-                  </span>
-                </a>
-                </Link>
-                <Link href="https://www.instagram.com/NovaMining/">
-                <a target="_blank" className="ls-social-icon has-text-white">
-                  <span className="icon is-large">
-                    <i className="fab fa-2x fa-instagram"></i>
-                  </span>
-                </a>
-                </Link>
-                <Link href="https://medium.com/novamining">
-                <a target="_blank" className="ls-social-icon has-text-white">
-                  <span className="icon is-large">
-                    <i className="fab fa-2x fa-medium"></i>
-                  </span>
-                </a>
-                </Link>
-                <Link href="https://github.com/novamining-network">
-                <a target="_blank" className="ls-social-icon has-text-white">
-                  <span className="icon is-large">
-                    <i className="fab fa-2x fa-github"></i>
-                  </span>
-                </a>
-                </Link>
-                <Link href="https://novamining-network.slack.com">
-                <a target="_blank" className="ls-social-icon has-text-white">
-                  <span className="icon is-large">
-                    <i className="fab fa-2x fa-slack"></i>
-                  </span>
-                </a>
-                </Link>
-                <Link href="https://t.me/NovaMining">
-                <a target="_blank" className="ls-social-icon has-text-white">
-                  <span className="icon is-large">
-                    <i className="fab fa-2x fa-telegram-plane"></i>
-                  </span>
-                </a>
-                </Link>
-              </p>
+            <div className"column flex-vertical-center">
+              <h2 className="subtitle is-4 has-text-white has-text-centered">
+                Our website is currently down for maintenance 🤓 We're working hard to bring it back online as soon as possible 🚀
+              </h2>
+              <h2 className="subtitle is-5 has-text-white has-text-centered">
+                Do you need any further information or do you have any enquiry? 🤔 Feel free to write us at <Link href="mailto:contact@novamining.io"><a className="has-text-white">contact@novamining.io</a></Link> 🤗
+              </h2>
+              <p className="column has-text-centered-mobile">
+                  <Link href="https://twitter.com/NovaMining_Net">
+                  <a target="_blank" className="ls-social-icon has-text-white">
+                    <span className="icon is-large">
+                      <i className="fab fa-2x fa-twitter"></i>
+                    </span>
+                  </a>
+                  </Link>
+                  <Link href="https://www.linkedin.com/company/novaminingnetwork">
+                  <a target="_blank" className="ls-social-icon has-text-white">
+                    <span className="icon is-large">
+                      <i className="fab fa-2x fa-linkedin-in"></i>
+                    </span>
+                  </a>
+                  </Link>
+                  <Link href="https://www.facebook.com/novamining.network">
+                  <a target="_blank" className="ls-social-icon has-text-white">
+                    <span className="icon is-large">
+                      <i className="fab fa-2x fa-facebook-f"></i>
+                    </span>
+                  </a>
+                  </Link>
+                  <Link href="https://www.instagram.com/NovaMining/">
+                  <a target="_blank" className="ls-social-icon has-text-white">
+                    <span className="icon is-large">
+                      <i className="fab fa-2x fa-instagram"></i>
+                    </span>
+                  </a>
+                  </Link>
+                  <Link href="https://medium.com/novamining">
+                  <a target="_blank" className="ls-social-icon has-text-white">
+                    <span className="icon is-large">
+                      <i className="fab fa-2x fa-medium"></i>
+                    </span>
+                  </a>
+                  </Link>
+                  <Link href="https://github.com/novamining-network">
+                  <a target="_blank" className="ls-social-icon has-text-white">
+                    <span className="icon is-large">
+                      <i className="fab fa-2x fa-github"></i>
+                    </span>
+                  </a>
+                  </Link>
+                  <Link href="https://novamining-network.slack.com">
+                  <a target="_blank" className="ls-social-icon has-text-white">
+                    <span className="icon is-large">
+                      <i className="fab fa-2x fa-slack"></i>
+                    </span>
+                  </a>
+                  </Link>
+                  <Link href="https://t.me/NovaMining">
+                  <a target="_blank" className="ls-social-icon has-text-white">
+                    <span className="icon is-large">
+                      <i className="fab fa-2x fa-telegram-plane"></i>
+                    </span>
+                  </a>
+                  </Link>
+                </p>
+              </div>
           </div>
         </div>
       </section>
