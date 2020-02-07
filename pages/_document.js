effa9e41596ec8690fcc3c6dedc07b5d6e272299
rand0mbyte <rand0mbyte@MacBook-Pro.local>
2020-02-07 22:17:27 +0100

    Update #2 02.07.2020 - Dependencies upgrade to latest releases

--- pages/_document.js
+++ pages/_document.js
@@ -14,20 +14,20 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
           <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="msapplication-TileImage" content="../static/mstile-144x144.png" />
+          <meta name="msapplication-TileImage" content="../public/static/mstile-144x144.png" />
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css" integrity="sha384-rtJEYb85SiYWgfpCr0jn174XgJTn4rptSOQsMroFBPQSGLdOC5IbubP6lJ35qoM9" crossOrigin="anonymous" />
-          <link rel="shortcut icon" type="image/x-icon" href="../static/favicon.ico" />
-          <link rel="icon" type="image/png" href="../static/favicon-16x16.png" sizes="16x16" />
-          <link rel="icon" type="image/png" href="../static/favicon-32x32.png" sizes="32x32" />
-          <link rel="icon" type="image/png" href="../static/favicon-96x96.png" sizes="96x96" />
-          <link rel="apple-touch-icon" sizes="57x57" href="../static/apple-touch-icon-57x57.png" />
-          <link rel="apple-touch-icon" sizes="114x114" href="../static/apple-touch-icon-114x114.png" />
-          <link rel="apple-touch-icon" sizes="72x72" href="../static/apple-touch-icon-72x72.png" />
-          <link rel="apple-touch-icon" sizes="144x144" href="../static/apple-touch-icon-144x144.png" />
-          <link rel="apple-touch-icon" sizes="60x60" href="../static/apple-touch-icon-60x60.png" />
-          <link rel="apple-touch-icon" sizes="120x120" href="../static/apple-touch-icon-120x120.png" />
-          <link rel="apple-touch-icon" sizes="76x76" href="../static/apple-touch-icon-76x76.png" />
-          <link rel="apple-touch-icon" sizes="152x152" href="../static/apple-touch-icon-152x152.png" />
+          <link rel="shortcut icon" type="image/x-icon" href="../public/static/favicon.ico" />
+          <link rel="icon" type="image/png" href="../public/static/favicon-16x16.png" sizes="16x16" />
+          <link rel="icon" type="image/png" href="../public/static/favicon-32x32.png" sizes="32x32" />
+          <link rel="icon" type="image/png" href="../public/static/favicon-96x96.png" sizes="96x96" />
+          <link rel="apple-touch-icon" sizes="57x57" href="../public/static/apple-touch-icon-57x57.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="../public/static/apple-touch-icon-114x114.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="../public/static/apple-touch-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="../public/static/apple-touch-icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="../public/static/apple-touch-icon-60x60.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="../public/static/apple-touch-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="../public/static/apple-touch-icon-76x76.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="../public/static/apple-touch-icon-152x152.png" />
         </Head>
         <body>
           <Main />
